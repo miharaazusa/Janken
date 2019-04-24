1c35af2f0c25a61f17124777b9e666463e420abc
mihara <49392483+miharaazusa@users.noreply.github.com>
2019-04-24 14:56:44 +0900

    Add files via upload

---
+++ janken.java
@@ -0,0 +1,24 @@
+package Hello;
+
+import java.util.Scanner;
+
+public class janken {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("グーは1、チョキは2、パーは3を出してください");
+
+		System.out.println("先手入力してください");
+		int a = sc.nextInt();
+		System.out.print("後手入力してください");
+		int b = sc.nextInt();
+
+		if(a==b) {
+			System.out.print("あいこです");
+		}else if(a+1==b || a==3&&b==1)
+		System.out.println("先手の勝ちです");
+	else {
+		System.out.println("後手の勝ちです");
+		}
+
+		}
+}
